1dd01c9a395800e353808c14d668cf9b017ef806
Maxime CHAMIS <maxime.chamis@gmail.com>
2025-02-16 15:45:36 +0100

    Updated script.sh with webhook test

--- script1.sh
+++ script1.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "Hello from my GitHub script!"
+echo "Hello, Jenkins! 🚀"
+echo "Today's date is: $(date)"
+echo "Running on machine: $(hostname)"
+echo "The current directory is: $(pwd)"
+echo "✅ This is an automated build triggered by GitHub Webhook!"
+
 
